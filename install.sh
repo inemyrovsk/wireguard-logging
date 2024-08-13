1ae72441b8ee2bb58edf4cae0806cdd5c9c15efe
Ivan Nemyrovskiy <99128310+inemyrovsk@users.noreply.github.com>
2024-08-13 14:35:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 echo "Installing please wait..."
 cp wireguard-logging.service /etc/systemd/system/wireguard-logging.service
-mkdir -p /etc/wireguard/clients/
-cp create_client.sh /etc/wireguard/
+
+
 cp wireguard-logging.sh /etc/wireguard/
+cp rotate_wireguard_logs.sh /usr/local/bin/rotate_wireguard_logs.sh
 sleep 5
-chmod +x /etc/wireguard/create_client.sh
+
+chmod +x /usr/local/bin/rotate_wireguard_logs.sh
 chmod +x /etc/wireguard/wireguard-logging.sh
 systemctl enable wireguard-logging.service
 systemctl start wireguard-logging.service
+sudo su
+(crontab -l ; echo "0 0 * * * /usr/local/bin/rotate_wireguard_logs.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 echo "Installation Complete !"
 echo "Please run ./create_client.sh to create new clients "
 echo "Log files are located at /var/log/wireguard/"
